3ff0d29455cea6e44b56d628ee3f93232e8c9055
Syed Shahiq <shahiqurrehman@gmail.com>
2020-09-14 11:56:58 +0500

    Customize realtor page for admin

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,4 +1,18 @@
+import datetime
+
 from django.contrib import admin
+
 from .models import Realtor
 
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'email', 'get_hire_date')
+    list_display_links = ('id', 'name')
+    ordering = ('id',)
+
+    def get_hire_date(self, obj):
+        date = obj.hire_date.strftime("%d %b %Y ")
+        return date
+    get_hire_date.short_description = 'Hire Date'
+
+admin.site.register(Realtor, RealtorAdmin)
