4802ee783cb5453b25a50fc0e6150f553ee9adbc
Syed Shahiq <shahiqurrehman@gmail.com>
2020-09-14 22:34:46 +0500

    Search functionality added

--- listings/admin.py
+++ listings/admin.py
@@ -5,11 +5,11 @@ from .models import Listing
 
 class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published',
-                    'price', 'list_date', 'realtor')
+                    'price', 'list_date', 'realtor','bedrooms')
     list_display_links = ('id', 'title')
     ordering = ('id',)
     list_filter = ('realtor', 'is_published')
-    list_editable = ('is_published', 'price')
+    list_editable = ('is_published', 'price','bedrooms')
     search_fields = ('title', 'description', 'address', 'city')
     list_per_page = 25
 
--- listings/views.py
+++ listings/views.py
@@ -27,9 +27,31 @@ def listing(request, listing_id):
 
 
 def search(request):
+    query_set = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_set = query_set.filter(description__icontains=keywords)
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_set = query_set.filter(city__iexact=city)
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_set = query_set.filter(state__iexact=state)
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_set = query_set.filter(bedrooms__lte=bedrooms)
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_set = query_set.filter(price__lte=price)
     context = {
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
-        'state_choices': state_choices
+        'state_choices': state_choices,
+        'listings': query_set
     }
-    return render(request, 'listings/search.html',context)
+    return render(request, 'listings/search.html', context)
