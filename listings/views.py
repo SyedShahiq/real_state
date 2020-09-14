bf65d8d643dfa90a71b19155e159959fe8754b36
Syed Shahiq <shahiqurrehman@gmail.com>
2020-09-14 13:07:14 +0500

    Properties listings added

--- listings/views.py
+++ listings/views.py
@@ -4,13 +4,13 @@ from .models import Listing
 
 def index(request):
     listings = Listing.objects.all()
-    print(listings)
-    return render(request, 'listings/listings.html', {
+    context = {
         'listings': listings
-    })
+    }
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
