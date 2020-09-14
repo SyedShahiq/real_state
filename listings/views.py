1ba6b18126e3a95dffb8d9f038aa5437deca0e48
Syed Shahiq <shahiqurrehman@gmail.com>
2020-09-14 13:57:15 +0500

    Pagination added

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator
 
 
 def index(request):
     listings = Listing.objects.all()
+    paginator = Paginator(listings, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'listings': listings
+        'listings': page_obj
     }
     return render(request, 'listings/listings.html', context)
 
