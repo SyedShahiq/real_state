f23cb36a3388e52e272da3e38b27b902eb8dea28
Syed Shahiq <shahiqurrehman@gmail.com>
2020-09-15 17:57:21 +0500

    Registration added

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
-        messages.success(request, 'Dekha na')
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'The user name already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'The email already exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(
+                        username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+                    user.save()
+                    # auth.login(username)
+                    messages.success(request, 'You are now registerd and can login')
+                    return redirect('login')
+        else:
+            messages.error(request, "Password don't match")
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
