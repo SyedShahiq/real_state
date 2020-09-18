da362d36409dcc64395cf656c61b5efa83c69fa7
Syed Shahiq <shahiqurrehman@gmail.com>
2020-09-18 11:21:08 +0500

    Models and migrations added

--- btre/settings.py
+++ btre/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
-    'django.contrib.humanize'
+    'django.contrib.humanize',
+    'contacts.apps.ContactsConfig'
 ]
 
 MIDDLEWARE = [
