3e8c79fda3544c90d18b04a6d40ae0c75ba5bbb5
Syed Shahiq <shahiqurrehman@gmail.com>
2020-09-18 14:49:35 +0500

    Inquiry page made functional

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .models import Contact
+
+
 def contact(request):
-    return
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+        contact = Contact(listing=listing, listing_id=listing_id,
+                          name=name, email=email, message=message, user_id=user_id)
+        contact.save()
+        messages.success(request, 'Inquiry added successfully')
+        return redirect('/listing/'+listing_id)
